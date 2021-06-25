8ff08f10551af3ec2b20ca10e64bf1fdacc6950a
parsley9877 <parsaassadi2@gmail.com>
2021-06-24 19:58:25 -0700

    added flag validators

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from absl import flags
 2 -- add BUILD file with your build rules
 3 -- use bazel to build --> bazel build: app
 4 -- use bazel to execute the generated executable --> bazel-bin/app --name=Daenerys --num=10
+
+Note -- use 'bazel-bin/app --help' for help on flags
 """
 flags.DEFINE_string('name', None, 'Username')
 flags.DEFINE_integer('num', 1, 'number of times')
@@ -14,8 +16,16 @@ flags.DEFINE_integer('num', 1, 'number of times')
 # Must be provided by user
 flags.mark_flag_as_required('name')
 
+# flag validators
+def num_checker(number):
+    return number % 3 == 0
+
+# assigning flag validator
+flags.register_validator('num', checker=num_checker, message='Number of times must be divisable by 3')
+
 # Entry func of program
 def main(argv):
+    # delete argv if you are not going to use it, or you will get error
     del argv
 
     for i in range(0, flags.FLAGS.num):
